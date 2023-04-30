89ed5516930b277a6f4b0a60a2e84484f4dd3d03
Ruslan Khalitov <ruslan2439@gmail.com>
2023-05-01 01:41:03 +0200

    remove old

--- experiments/data/genbank_parse_category.py
+++
@@ -1,6 +0,0 @@
-import genbank_taxonomy
-
-
-genbank_path = '.'
-for category in ['Bacterial', 'Invertebrate', 'Other mammalian', 'Plant', 'Primate', 'Rodent', 'Viral', 'Other vertebrate']:
-    genbank_taxonomy.parse_category(category, genbank_path, dump_seqs=True)
